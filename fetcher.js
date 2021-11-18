d05161eb8513afd7821fb73ce91bf9bf58b88b52
Abhiram <abhiram.satha@gmail.com>
2021-11-18 13:12:22 +0000

    Added the request funcest function

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const request = require('request');
 
 const commandArg = process.argv.slice(2);
 
@@ -7,12 +7,21 @@ const fileName = commandArg[1];
 
 const dataWebsite = commandArg[0];
 
+request(dataWebsite, (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
+
+  fs.writeFile(fileName, body, 'UTF8', (err) => {
+    if (err) {
+      return console.log('err');
+    } else {
+      const stats = fs.statSync(fileName);
+      const fileSizeInBytes = stats.size;
+      console.log(fileSizeInBytes);
+      console.log(`Downloaded and saved ${fileSizeInBytes} btyes to ${fileName}`);
+    }
+  })
+});
 
 
-// fs.writeFile(fileName, 'hello', 'UTF8', (err) => {
-//   if (err) {
-//     return console.log('err');
-//   } else {
-//     console.log('it has been saved');
-//   }
-// })
